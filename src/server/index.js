90e7b155f7df31c9629ff8780e875f6fbd7f676f
Jordan Gillard <jordan-gillard@outlook.com>
2020-04-06 19:00:47 +0200

    Makes base server and adds start-server to node scripts

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,32 @@
-const AYLIEN_API_KEY = process.env.AYLIEN_API_KEY;
-const AYLIEN_APP_ID = process.env.AYLIEN_APP_ID;
+const AYLIENTextAPI = require('aylien_textapi');
+const express = require('express');
+const app = express();
+const port = 3000;
 
+const AYLIEN_APP_ID = process.env.AYLIEN_APP_ID;
+const AYLIEN_API_KEY = process.env.AYLIEN_API_KEY;
+
+const textapi = new AYLIENTextAPI({
+    application_id: AYLIEN_APP_ID,
+    application_key: AYLIEN_API_KEY
+});
+
+
+function getTextSentiment(text) {
+    textapi.sentiment({
+        'text': text
+    }, function (error, response) {
+        if (error === null) {
+            console.log(response);
+        }
+    })
+}
+
+
+app.post('/', function(req, res) {
+    console.log(req.data);
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+});
